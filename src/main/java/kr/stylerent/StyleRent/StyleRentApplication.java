5b4abc75d04bfa8ce8962f36d6ecfd27e1848d26
Alisherka7 <alisherka7@jj.ac.kr>
2023-03-23 20:25:17 +0900

    v0.1

---
+++ src/main/java/kr/stylerent/StyleRent/StyleRentApplication.java
@@ -0,0 +1,13 @@
+package kr.stylerent.StyleRent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyleRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StyleRentApplication.class, args);
+	}
+
+}
